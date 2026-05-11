5944a90e51487f54292ea74a7f66cb588e3541a8
alex <ursusandwolf@protonmail.com>
2026-05-11 18:41:29 +0300

    it's works

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    public static final int MAX = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +14,18 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        int length = finishPoint - startPoint; // Это и есть ваш workLoad
+        if (length <= MAX) {
+            long sum = 0;
+            for (int i = startPoint; i < finishPoint; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+        int middle = startPoint + (finishPoint - startPoint) / 2;
+        MyTask leftTask = new MyTask(startPoint, middle);
+        MyTask rightTask = new MyTask(middle, finishPoint);
+        leftTask.fork(); // Запускаем одну задачу асинхронно
+        return rightTask.compute() + leftTask.join();
     }
 }
